1d233db01eba7b8aae1e9433f18f30e4b2d7410a
Lucas Amaral Oliveira da Silva <lucas.amaral.oliveira.silva@gmail.com>
2021-11-01 23:23:11 -0300

    Primeiro Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
+}
